574895f8f3f9d9f5983da5833d4e1d8c2a7cac69
metherton <martinetherton63@gmail.com>
2019-11-20 22:43:24 +0100

    Streaming random numbers

--- src/main/scala/martinetherton/WebServer.scala
+++ src/main/scala/martinetherton/WebServer.scala
@@ -1,77 +1,41 @@
 package martinetherton
 
 import akka.actor.ActorSystem
+import akka.stream.scaladsl._
+import akka.util.ByteString
 import akka.http.scaladsl.Http
-import akka.stream.ActorMaterializer
-import akka.Done
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.model.{ HttpEntity, ContentTypes }
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.model.StatusCodes
-// for JSON serialization/deserialization following dependency is required:
-// "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.7"
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import spray.json.DefaultJsonProtocol._
-
+import akka.stream.ActorMaterializer
+import scala.util.Random
 import scala.io.StdIn
-
-import scala.concurrent.Future
 
 object WebServer {
 
-  // needed to run the route
-  implicit val system = ActorSystem()
-  implicit val materializer = ActorMaterializer()
-  // needed for the future map/flatmap in the end and future in fetchItem and saveOrder
-  implicit val executionContext = system.dispatcher
-
-  var orders: List[Item] = Nil
-
-  // domain model
-  final case class Item(name: String, id: Long)
-  final case class Order(items: List[Item])
-
-  // formats for unmarshalling and marshalling
-  implicit val itemFormat = jsonFormat2(Item)
-  implicit val orderFormat = jsonFormat1(Order)
-
-  // (fake) async database query api
-  def fetchItem(itemId: Long): Future[Option[Item]] = Future {
-    orders.find(o => o.id == itemId)
-  }
-  def saveOrder(order: Order): Future[Done] = {
-    orders = order match {
-      case Order(items) => items ::: orders
-      case _            => orders
-    }
-    Future { Done }
-  }
-
   def main(args: Array[String]) {
 
-    val route: Route =
-      concat(
-        get {
-          pathPrefix("item" / LongNumber) { id =>
-            // there might be no item for a given id
-            val maybeItem: Future[Option[Item]] = fetchItem(id)
+    implicit val system = ActorSystem()
+    implicit val materializer = ActorMaterializer()
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher
 
-            onSuccess(maybeItem) {
-              case Some(item) => complete(item)
-              case None       => complete(StatusCodes.NotFound)
-            }
-          }
-        },
-        post {
-          path("create-order") {
-            entity(as[Order]) { order =>
-              val saved: Future[Done] = saveOrder(order)
-              onComplete(saved) { done =>
-                complete("order created")
-              }
-            }
-          }
+    // streams are re-usable so we can define it here
+    // and use it for every request
+    val numbers = Source.fromIterator(() =>
+      Iterator.continually(Random.nextInt()))
+
+    val route =
+      path("random") {
+        get {
+          complete(
+            HttpEntity(
+              ContentTypes.`text/plain(UTF-8)`,
+              // transform each number to a chunk of bytes
+              numbers.map(n => ByteString(s"$n\n"))
+            )
+          )
         }
-      )
+      }
 
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
@@ -79,6 +43,5 @@ object WebServer {
     bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
       .onComplete(_ => system.terminate()) // and shutdown when done
-
   }
 }
