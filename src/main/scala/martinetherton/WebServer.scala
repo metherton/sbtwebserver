30f88d8e6b23781c4d443f9c87cd2887b0f19218
metherton <martinetherton63@gmail.com>
2020-04-04 17:08:28 +0200

    added factorial

--- src/main/scala/martinetherton/WebServer.scala
+++ src/main/scala/martinetherton/WebServer.scala
@@ -3,21 +3,15 @@ package martinetherton
 // "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.7"
 // for JSON serialization/deserialization following dependency is required:
 // "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.7"
-import java.sql.Timestamp
+import java.nio.file.Paths
 
-import akka.{Done, NotUsed}
 import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.server.Directives._
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Source
-import spray.json.DefaultJsonProtocol._
-import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
-import spray.json.{DeserializationException, JsNumber, JsValue, JsonFormat}
+import akka.stream.IOResult
+import akka.stream.scaladsl.{FileIO, Source}
+import akka.util.ByteString
+import akka.{Done, NotUsed}
 
 import scala.concurrent.Future
-import scala.io.StdIn
 
 object WebServer extends App {
 
@@ -39,13 +33,23 @@ object WebServer extends App {
 //  // needed for the future flatMap/onComplete in the end
 //  implicit val executionContext = system.dispatcher
 
+
+  implicit val system = ActorSystem("QuickStart")
   val source: Source[Int, NotUsed] = Source(1 to 100)
+  source.runForeach(i => println(i))
+
+  val factorials = source.scan(BigInt(1))((acc, next) => acc * next)
+
+  val result: Future[IOResult] =
+    factorials.map(num => ByteString(s"$num\n")).runWith(FileIO.toPath(Paths.get("factorials.txt")))
+
+
   val done: Future[Done] = source.runForeach(i => println(i))
 
   implicit val ec = system.dispatcher
   done.onComplete(_ => system.terminate())
 
-  val repo = new PersonRepository
+//  val repo = new PersonRepository
 
 //  val route =
 //    path("items") {
@@ -72,42 +76,42 @@ object WebServer extends App {
 //      )
 //    }
 
-  val route1 = cors() {
-    path("persons") {
-      concat(
-        get {
-
-          import martinetherton.PersonRepository
-          //     val messagesResults = result(messagesFuture, 2.seconds)
-          //          val sql = messages.result.statements.mkString
-
-          val result = repo.getPersons
-          complete(result)
-
-        },
-        post {
-          entity(as[Person]) { person =>
-            //         def newMessage = Seq(
-            //           Message("Martin", "This is my new message")
-            //         )
-
-            val insAct = repo.insert(person)
-            onComplete(insAct) { done =>
-              complete(s"new person added with id: ${insAct}")
-            }
-          }
-        }
-      )
-    }
-
-  }
-
-  val bindingFuture = Http().bindAndHandle(route1, "0.0.0.0", 8080)
-
-  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-  StdIn.readLine() // let it run until user presses return
-  bindingFuture
-    .flatMap(_.unbind()) // trigger unbinding from the port
-    .onComplete(_ => system.terminate()) // and shutdown when done
+//  val route1 = cors() {
+//    path("persons") {
+//      concat(
+//        get {
+//
+//          import martinetherton.PersonRepository
+//          //     val messagesResults = result(messagesFuture, 2.seconds)
+//          //          val sql = messages.result.statements.mkString
+//
+//          val result = repo.getPersons
+//          complete(result)
+//
+//        },
+//        post {
+//          entity(as[Person]) { person =>
+//            //         def newMessage = Seq(
+//            //           Message("Martin", "This is my new message")
+//            //         )
+//
+//            val insAct = repo.insert(person)
+//            onComplete(insAct) { done =>
+//              complete(s"new person added with id: ${insAct}")
+//            }
+//          }
+//        }
+//      )
+//    }
+//
+//  }
+//
+//  val bindingFuture = Http().bindAndHandle(route1, "0.0.0.0", 8080)
+//
+//  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+//  StdIn.readLine() // let it run until user presses return
+//  bindingFuture
+//    .flatMap(_.unbind()) // trigger unbinding from the port
+//    .onComplete(_ => system.terminate()) // and shutdown when done
 
 }
