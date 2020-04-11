17b078113733c1d78f6ac7d66886a267dcfc73de
metherton <martinetherton63@gmail.com>
2020-04-11 22:22:58 +0200

    use success and failure handlers of future

--- src/main/scala/martinetherton/WebServer.scala
+++ src/main/scala/martinetherton/WebServer.scala
@@ -3,123 +3,108 @@ package martinetherton
 // "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.7"
 // for JSON serialization/deserialization following dependency is required:
 // "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.7"
-import java.nio.file.{Paths, StandardOpenOption}
+import java.sql.Timestamp
 
 import akka.actor.ActorSystem
-import akka.stream.IOResult
-import akka.stream.scaladsl.{FileIO, Sink, Source}
-import akka.util.ByteString
-import akka.{Done, NotUsed}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import spray.json.DefaultJsonProtocol._
+import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
+import spray.json.{DeserializationException, JsNumber, JsValue, JsonFormat}
 
-import scala.concurrent.Future
+import scala.io.StdIn
+import scala.util.{Failure, Success}
 
 object WebServer extends App {
 
-//  import slick.jdbc.H2Profile.api._
-//
-//  implicit object TimestampFormat extends JsonFormat[Timestamp] {
-//    def write(obj: Timestamp) = JsNumber(obj.getTime)
-//
-//    def read(json: JsValue) = json match {
-//      case JsNumber(time) => new Timestamp(time.toLong)
-//
-//      case _ => throw new DeserializationException("Date expected")
-//    }
-//  }
-//  implicit val messageFormat = jsonFormat7(Person)
-//
-//  implicit val system = ActorSystem("my-system")
-//  implicit val materializer = ActorMaterializer()
-//  // needed for the future flatMap/onComplete in the end
+  import slick.jdbc.H2Profile.api._
 
+  implicit object TimestampFormat extends JsonFormat[Timestamp] {
+    def write(obj: Timestamp) = JsNumber(obj.getTime)
 
+    def read(json: JsValue) = json match {
+      case JsNumber(time) => new Timestamp(time.toLong)
 
-  implicit val system = ActorSystem("QuickStart")
+      case _ => throw new DeserializationException("Date expected")
+    }
+  }
+  implicit val messageFormat = jsonFormat7(Person)
 
- // val source: Source[Int, NotUsed] = Source(1 to 100)
-
-
+  implicit val system = ActorSystem("my-system")
+  implicit val materializer = ActorMaterializer()
+  // needed for the future flatMap/onComplete in the end
   implicit val executionContext = system.dispatcher
 
-  val file = Paths.get("test.csv")
+  val repo = new PersonRepository
 
-  val outfile = Paths.get("greeting.txt")
-  private val mySource: Source[ByteString, Future[IOResult]] = FileIO.fromPath(file)
+  //  val route =
+  //    path("items") {
+  //      concat(
+  //        get {
+  //          val messagesAction: DBIO[Seq[Message]] = messages.result
+  //          val messagesFuture: Future[Seq[Message]] = db.run(messagesAction)
+  //
+  //     //     val messagesResults = result(messagesFuture, 2.seconds)
+  ////          val sql = messages.result.statements.mkString
+  //          complete(messagesFuture)
+  //
+  //        },
+  //        post {
+  //          def newMessage = Seq(
+  //            Message("Martin", "This is my new message")
+  //          )
+  //          val ins: DBIO[Option[Int]] = messages ++= newMessage
+  //          val insAct: Future[Option[Int]] = db.run(ins)
+  //          onComplete(insAct) { done =>
+  //            complete("new message added")
+  //          }
+  //        }
+  //      )
+  //    }
 
-  println(mySource.toString())
-  private val eventualResult: Future[IOResult] = mySource.runWith(FileIO.toPath(outfile, Set(StandardOpenOption.APPEND)))
-  eventualResult.onComplete(_ => system.terminate())
+  val route1 = cors() {
+    path("persons") {
+      concat(
+        get {
 
-//  val outfile = Paths.get("greeting.txt")
-//  val text = Source.single("Hello Akka Stream!")
-//  val result: Future[IOResult] = text.map(t => ByteString(t)).runWith(FileIO.toPath(outfile))
-//  val done: Future[Done] = source.runForeach(i => println(i))
+          import martinetherton.PersonRepository
+          //     val messagesResults = result(messagesFuture, 2.seconds)
+          //          val sql = messages.result.statements.mkString
 
-//  done.onComplete(_ => system.terminate())
+          val result = repo.getPersons
+          onComplete(result) {
+            case Success(persons) => complete(persons)
+            case Failure(exception) => failWith(exception)
+          }
+          complete(result)
 
-//  val repo = new PersonRepository
+        },
+        post {
+          entity(as[Person]) { person =>
+            //         def newMessage = Seq(
+            //           Message("Martin", "This is my new message")
+            //         )
 
-//  val route =
-//    path("items") {
-//      concat(
-//        get {
-//          val messagesAction: DBIO[Seq[Message]] = messages.result
-//          val messagesFuture: Future[Seq[Message]] = db.run(messagesAction)
-//
-//     //     val messagesResults = result(messagesFuture, 2.seconds)
-////          val sql = messages.result.statements.mkString
-//          complete(messagesFuture)
-//
-//        },
-//        post {
-//          def newMessage = Seq(
-//            Message("Martin", "This is my new message")
-//          )
-//          val ins: DBIO[Option[Int]] = messages ++= newMessage
-//          val insAct: Future[Option[Int]] = db.run(ins)
-//          onComplete(insAct) { done =>
-//            complete("new message added")
-//          }
-//        }
-//      )
-//    }
+            val insAct = repo.insert(person)
+            onComplete(insAct) { done =>
+              complete(s"new person added with id: ${insAct}")
+            }
+          }
+        }
+      )
+    }
 
-//  val route1 = cors() {
-//    path("persons") {
-//      concat(
-//        get {
-//
-//          import martinetherton.PersonRepository
-//          //     val messagesResults = result(messagesFuture, 2.seconds)
-//          //          val sql = messages.result.statements.mkString
-//
-//          val result = repo.getPersons
-//          complete(result)
-//
-//        },
-//        post {
-//          entity(as[Person]) { person =>
-//            //         def newMessage = Seq(
-//            //           Message("Martin", "This is my new message")
-//            //         )
-//
-//            val insAct = repo.insert(person)
-//            onComplete(insAct) { done =>
-//              complete(s"new person added with id: ${insAct}")
-//            }
-//          }
-//        }
-//      )
-//    }
-//
-//  }
-//
-//  val bindingFuture = Http().bindAndHandle(route1, "0.0.0.0", 8080)
-//
-//  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-//  StdIn.readLine() // let it run until user presses return
-//  bindingFuture
-//    .flatMap(_.unbind()) // trigger unbinding from the port
-//    .onComplete(_ => system.terminate()) // and shutdown when done
+  }
+
+  val bindingFuture = Http().bindAndHandle(route1, "0.0.0.0", 8080)
+
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
 
 }
