0cbb40a2849d915c2093c3f054eea89a0282988a
metherton <martinetherton63@gmail.com>
2020-05-03 22:38:20 +0200

    parse bytes per line

--- src/main/scala/martinetherton/WebServer.scala
+++ src/main/scala/martinetherton/WebServer.scala
@@ -3,13 +3,16 @@ package martinetherton
 // "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.7"
 // for JSON serialization/deserialization following dependency is required:
 // "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.7"
+import java.nio.file.Paths
 import java.sql.Timestamp
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.server.Directives._
-import akka.stream.ActorMaterializer
+import akka.stream.{ActorMaterializer, IOResult}
+import akka.stream.scaladsl.{FileIO, Framing, Keep, Sink, Source}
+import akka.util.ByteString
 import spray.json.DefaultJsonProtocol._
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
 import spray.json.{DeserializationException, JsNumber, JsValue, JsonFormat}
@@ -64,6 +67,15 @@ object WebServer extends App {
   //      )
   //    }
 
+  val file = Paths.get("/Users/martin/myprojects/sbt/mywebserver/server/src/main/scala/martinetherton/etherton-london-1.ged")
+  val source: Source[ByteString, Future[IOResult]] = FileIO.fromPath(file)
+
+  val linesStream = source.via(Framing.delimiter(
+    ByteString("\r\n"), maximumFrameLength = 500, allowTruncation = true))
+    .map(_.utf8String).runWith(Sink.seq)
+
+ // val future = source.map(bs => bs.utf8String).runWith(Sink.seq)
+
   val persons = Future{List(Person("martin", "etherton", new Timestamp(12121), "high street", "sheffield", "england"))}
 
   val route1 = //cors() {
@@ -76,7 +88,7 @@ object WebServer extends App {
           //          val sql = messages.result.statements.mkString
 
          // val result = repo.getPersons
-          complete(persons)
+          complete(linesStream)
 
         },
 //        post {
