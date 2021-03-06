8d2f7f4e5d0bf944abfd604f01aebbecd4cb9816
metherton <martinetherton63@gmail.com>
2021-03-06 22:57:46 +0100

    added server cert

--- src/main/scala/martinetherton/web/WebServer.scala
+++ src/main/scala/martinetherton/web/WebServer.scala
@@ -131,6 +131,7 @@ object WebServer extends App with Marshallers {
   val https: HttpsConnectionContext = ConnectionContext.httpsServer(sslContext)
 
   val bindingFuture = Http().newServerAt("0.0.0.0", 8443).enableHttps(https).bind(routing)
+  val bindingFuture1 = Http().newServerAt("0.0.0.0", 8080).bind(routing)
 
   println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
   StdIn.readLine() // let it run until user presses return
